11389a84a7eddf1eb174bbbe120f45ca5217f386
Ivan <ivanvolodin960@gmail.com>
2023-04-26 18:42:49 +0300

    feat: create component Days

---
+++ src/pages/Home/components/Days/Days.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Days.module.scss';
+
+interface Props {
+
+}
+
+export const Days = (props: Props) => {
+  return (
+    <div className={s.days}>
+      Component
+    </div>
+  )
+}
