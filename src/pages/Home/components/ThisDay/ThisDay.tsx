ebddcb76feeb4701afd3bd4072ceebcc263e66e9
Ivan <ivanvolodin960@gmail.com>
2023-04-26 13:57:42 +0300

    feat: add component ThisDay

--- src/pages/Home/components/ThisDay/ThisDay.tsx
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -8,7 +8,18 @@ interface Props {
 export const ThisDay = (props: Props) => {
   return (
     <div className={s.this__day}>
-      ThisDay
+      <div className={s.top__block}>
+        <div className={s.this__temp}>20°C</div>
+        <div className={s.this__day_day}>Today</div>
+      </div>
+      <div className={s.bottom__block}>
+        <div className={s.this__time}>
+          Time: <span>21:54</span>
+        </div>
+        <div className={s.this__city}>
+          City: <span>Saint-Petersburg</span>
+        </div>
+      </div>
     </div>
   )
 }
