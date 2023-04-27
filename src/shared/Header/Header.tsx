e6377f0117857831c8379bbf3be9af4b03164576
Ivan <ivanvolodin960@gmail.com>
2023-04-27 14:31:39 +0300

    refactor: change colors

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -3,7 +3,7 @@ import Select from "react-select";
 import { GlobalSvgSelector } from "../../assets/icons/global/GlobalSvgSelector";
 import s from "./Header.module.scss";
 
-interface Props {}
+interface Props { }
 
 export const Header = (props: Props) => {
   const options = [
@@ -15,12 +15,16 @@ export const Header = (props: Props) => {
   const colourStyles = {
     control: (styles: any) => ({
       ...styles,
-      backgroundColor: "rgba(71, 147, 255, 0.2)",
+      backgroundColor: 0 ? "#4f4f4f" : "rgba(71, 147, 255, 0.2)",
       width: "194px",
       height: "37px",
       border: "none",
       borderRadius: "10px",
       zIndex: 100,
+    }),
+    singleValue: (styles: any) => ({
+      ...styles,
+      color: 0 ? "#fff" : "#000",
     }),
   };
 
@@ -36,10 +40,10 @@ export const Header = (props: Props) => {
         <div className={s.change__theme}>
           <GlobalSvgSelector id="change-theme" />
         </div>
-        <Select 
+        <Select
           defaultValue={options[0]}
-          options={options} 
-          styles={colourStyles} 
+          options={options}
+          styles={colourStyles}
         />
       </div>
     </header>
