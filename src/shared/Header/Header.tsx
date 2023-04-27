4c4dcb1d9247cdb2e97ee93fb6cc2d7816fd4616
Ivan <ivanvolodin960@gmail.com>
2023-04-27 15:38:24 +0300

    feat: add logics to change theme

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Select from "react-select";
 import { GlobalSvgSelector } from "../../assets/icons/global/GlobalSvgSelector";
 import s from "./Header.module.scss";
@@ -12,10 +12,12 @@ export const Header = (props: Props) => {
     { value: "city-3", label: "Novgorod" },
   ];
 
+  const [theme, setTheme] = useState('light');
+
   const colourStyles = {
     control: (styles: any) => ({
       ...styles,
-      backgroundColor: 0 ? "#4f4f4f" : "rgba(71, 147, 255, 0.2)",
+      backgroundColor: theme === 'dark' ? "#4f4f4f" : "rgba(71, 147, 255, 0.2)",
       width: "194px",
       height: "37px",
       border: "none",
@@ -24,9 +26,29 @@ export const Header = (props: Props) => {
     }),
     singleValue: (styles: any) => ({
       ...styles,
-      color: 0 ? "#fff" : "#000",
+      color: theme === 'dark' ? "#fff" : "#000",
     }),
   };
+
+  function changeTheme() {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  useEffect(() => {
+    const root = document.querySelector(':root') as HTMLElement;
+
+    const components = [
+      'body-background',
+      'components-background',
+      'card-background',
+      'card-shadow',
+      'text-color'
+    ];
+
+    components.forEach((component) => {
+      root.style.setProperty(`--${component}-default`, `var(--${component}-${theme})`);
+    });
+  }, [theme]);
 
   return (
     <header className={s.header}>
@@ -37,7 +59,7 @@ export const Header = (props: Props) => {
         <div className={s.title}>Weather App</div>
       </div>
       <div className={s.wrapper}>
-        <div className={s.change__theme}>
+        <div className={s.change__theme} onClick={changeTheme}>
           <GlobalSvgSelector id="change-theme" />
         </div>
         <Select
