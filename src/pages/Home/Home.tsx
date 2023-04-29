5c840574ddd3ed9dee724d32e012333f1c455427
Ivan <ivanvolodin960@gmail.com>
2023-04-29 13:36:48 +0300

    refactor: divide part of logics

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useCustomDispatch, useCustomSelector } from '../../hooks/store';
+import { selectCurrentWeatherData } from '../../store/selectors';
 import { fetchCurrentWeather } from '../../store/thunks/fetchCurrentWeather';
 import { Days } from './components/Days/Days';
 import { ThisDay } from './components/ThisDay/ThisDay';
@@ -13,7 +14,7 @@ interface Props {
 export const Home = (props: Props) => {
   const dispatch = useCustomDispatch();
 
-  const { weather } = useCustomSelector(state => state.currentWeatherSliceReducer);
+  const { weather } = useCustomSelector(selectCurrentWeatherData);
 
   useEffect(() => {
     dispatch(fetchCurrentWeather('paris'))
--- src/pages/Home/components/ThisDay/ThisDay.tsx
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -12,7 +12,7 @@ export const ThisDay = ({ weather }: Props) => {
     <div className={s.this__day}>
       <div className={s.top__block}>
         <div className={s.top__block_wrapper}>
-          <div className={s.this__temp}>{weather.main.temp}°C</div>
+          <div className={s.this__temp}>{Math.floor(weather.main.temp)}°C</div>
           <div className={s.this__day_name}>Today</div>
         </div>
         <GlobalSvgSelector id="sun" />
