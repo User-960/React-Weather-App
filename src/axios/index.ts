3f47b4b337e1f2cb43905dea7fb83dbe6600beb6
Ivan <ivanvolodin960@gmail.com>
2023-09-23 13:25:16 +0300

    feat: configure env for project

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
+import { API_KEY, SERVER_URL } from '../config/app.constants'
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: SERVER_URL,
 });
 
 api.interceptors.request.use(config => {
-  config.url = config.url + '&units=metric&cnt=14&appid=' + process.env.REACT_APP_API_KEY;
+  config.url = config.url + '&units=metric&cnt=14&appid=' + API_KEY;
   return config;
 });
 
